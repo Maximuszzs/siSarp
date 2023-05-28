ad31c96d0697c0578a57cba667a482657df27f79
Maximuszzs <makskartasov04@gmail.com>
2023-05-28 21:06:22 +0300

    шляпа

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -61,5 +61,76 @@ int Method3()
 
 //Вид 4
 
+//  string Method4(int count, string c);
+//  {
+//      int i = 0;
+//      string result = String.Empty;
+
+//      while (i < count)
+//      {
+//          result = result + text;
+//          i++;
+//      }
+//      return result;
+//  }
+
+//  string result = Method4(10, "z");
+//  Console.WriteLine(result);
 
 
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result + text;
+}
+// string result = Method4(10, "z");
+// Console.WriteLine(result);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленткие буквы "к" замеить большими "К" ,
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задаса?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю? ";
+
+// string s ="qwerty"
+//            012345
+// s[3] //r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//      string result = String.Empty;
+//      int length = text.Length;
+//      for (int i= 0; i < length; i++)
+//      {
+//         if(text[i] == oldValue) result + $"{newValue}";
+//         else result + $"{str[i]}";
+//      }
+//      return result;
+// }
